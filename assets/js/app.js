b723ccfdd048ec6f898294628bce4ede29f5eb60
LucFII <LSFIIChi@Gmail.com>
2017-06-22 17:47:34 -0500

    logic additions

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,5 +9,36 @@
   };
 
   firebase.initializeApp(config);
-  
   var database = firebase.database();
+
+// Input Field variables
+  var trainNameInput = "";
+  var destinationInput = "";
+  var firstTrainTimeInput = "";
+  var frequencyInput = "";
+
+
+// Other inputs - might not need 
+  var trainName = "";
+  var trainDestination = "";
+  var firstTrainTime = "";
+  var nextArrival = "";
+  var minutesAway = "";
+// --------
+
+$("#submitButton").on("click", function(event) {
+    event.preventDefault();
+
+    trainNameInput = $("#trainNameInput").val().trim();
+    trainDestinationInput = $("#destinationInput").val().trim();
+    firstTrainTimeInput = $("#firstTrainTimeInput").val().trim();
+    frequencyInput = $("#frequencyInput").val().trim();
+
+
+    database.ref().push({
+    trainName: trainNameInput,
+    trainDestination: destinationInput,
+    firstTrainTime: firstTrainTimeInput,
+    trainFrequency: frequencyInput,
+    });
+  });
