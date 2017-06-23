447d61b07c992185b6e08e70c803d838c508c037
LucFII <LSFIIChi@Gmail.com>
2017-06-22 20:57:56 -0500

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,6 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBtsVzA_sKCy7XzOwbeVzAHbPlMwohGKTU",
-    authDomain: "train-scheduler-wk7.firebaseapp.com",
-    databaseURL: "https://train-scheduler-wk7.firebaseio.com",
-    projectId: "train-scheduler-wk7",
-    storageBucket: "",
-    messagingSenderId: "940603114062"
+
   };
 
   firebase.initializeApp(config);
